5aab4d09b0aaab3c7b923944a0bf5ec326914b12
Damian <d@c64.ch>
2020-08-03 20:09:40 +0200

    Edit Product Additional

---
+++ C64.FrontEnd/Models/EditProductionAdditional.cs
@@ -0,0 +1,12 @@
+﻿using C64.Data.Entities;
+using System.Collections.Generic;
+
+namespace C64.FrontEnd.Models
+{
+    public class EditProductionAdditional
+    {
+        public string Remarks { get; set; }
+
+        public List<HiddenPart> HiddenParts { get; set; } = new List<HiddenPart>() { new HiddenPart(), new HiddenPart(), new HiddenPart};
+    }
+}
