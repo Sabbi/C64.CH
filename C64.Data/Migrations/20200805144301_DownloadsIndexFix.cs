2ceb6a37f717f20f29c85ba1670097660d5f3880
Damian <d@c64.ch>
2020-08-05 16:53:04 +0200

    Added Combined Index for Downloads, fixes #2

---
+++ C64.Data/Migrations/20200805144301_DownloadsIndexFix.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace C64.Data.Migrations
+{
+    public partial class DownloadsIndexFix : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Downloads_ProductionFileId_DownloadedOn",
+                table: "Downloads",
+                columns: new[] { "ProductionFileId", "DownloadedOn" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Downloads_ProductionFileId_DownloadedOn",
+                table: "Downloads");
+        }
+    }
+}
