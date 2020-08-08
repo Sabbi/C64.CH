5788cf1c2d535382b7cbac65f3037cc2e3dc1166
Damian <d@c64.ch>
2020-08-08 13:51:54 +0200

    Edit Pictures

---
+++ C64.FrontEnd/Models/EditProductionPictures.cs
@@ -0,0 +1,15 @@
+﻿using C64.Data.Entities;
+using System.Collections.Generic;
+
+namespace C64.FrontEnd.Models
+{
+    public class EditProductionPictures
+    {
+        public List<ProductionPictureExtended> ProductionPictures { get; set; } = new List<ProductionPictureExtended>();
+    }
+
+    public class ProductionPictureExtended : ProductionPicture
+    {
+        public string Base64Data { get; set; }
+    }
+}
