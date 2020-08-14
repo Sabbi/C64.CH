4b81e21cfa882e03ae68b81678db9f1897cd2f1e
Damian <d@c64.ch>
2020-08-14 18:15:31 +0200

    Account / Edit password

---
+++ C64.FrontEnd/Models/EditPasswordModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace C64.FrontEnd.Models
+{
+    public class EditPasswordModel
+    {
+        [MinLength(6)]
+        [MaxLength(255)]
+        [Required]
+        public string CurrentPassword { get; set; }
+
+        [MinLength(6)]
+        [MaxLength(255)]
+        [Required]
+        public string NewPassword { get; set; }
+
+        [MinLength(6)]
+        [MaxLength(255)]
+        [Required]
+        [Compare("NewPassword")]
+        public string ConfirmPassword { get; set; }
+    }
+}
