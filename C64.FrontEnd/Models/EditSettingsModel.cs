d97c7818c62bcc92ffc7970187f162a3977dfff8
Damian <d@c64.ch>
2020-08-15 10:38:36 +0200

    Edit settings / notifications

---
+++ C64.FrontEnd/Models/EditSettingsModel.cs
@@ -0,0 +1,28 @@
+﻿using C64.Data.Entities;
+
+namespace C64.FrontEnd.Models
+{
+    public class EditSettingsModel
+    {
+        public bool Newsletter { get; set; }
+        public bool Digest { get; set; }
+        public bool PublicRatings { get; set; }
+        public bool PublicFavorites { get; set; }
+
+        public void Load(User user)
+        {
+            Newsletter = user.Newsletter;
+            Digest = user.Digest;
+            PublicFavorites = user.PublicFavorites;
+            PublicRatings = user.PublicRatings;
+        }
+
+        public void Update(User user)
+        {
+            user.Newsletter = Newsletter;
+            user.Digest = Digest;
+            user.PublicFavorites = PublicFavorites;
+            user.PublicRatings = PublicRatings;
+        }
+    }
+}
