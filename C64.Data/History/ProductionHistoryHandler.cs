d43d0891919a5e2f4619f00c76efdc205346f121
Damian <d@c64.ch>
2020-08-30 18:13:54 +0200

    Add some generic to the HistoryHandler a litte less RY

--- C64.Data/History/ProductionHistoryHandler.cs
+++ C64.Data/History/ProductionHistoryHandler.cs
@@ -14,37 +14,22 @@ namespace C64.Data.History
         void Undo(IEnumerable<HistoryRecord> historiesToUndo);
     }
 
-    public class ProductionHistoryHandler : IHistoryHandler
+    public abstract class HistoryHandler<T> : IHistoryHandler
     {
-        private readonly IUnitOfWork unitOfWork;
-        private readonly Production production;
-        private readonly string userId;
-        private readonly string userIp;
-        private readonly List<HistoryRecord> history = new List<HistoryRecord>();
+        protected IUnitOfWork unitOfWork;
+        protected T entity;
+        protected string userId;
+        protected string userIp;
+        protected List<HistoryRecord> history = new List<HistoryRecord>();
 
-        public ProductionHistoryHandler(IUnitOfWork unitOfWork, Production production, string userId, string userIp)
+        public abstract void AddHistory(HistoryEditProperty property, object newValue, HistoryStatus status = HistoryStatus.ToApply);
+
+        public HistoryHandler(IUnitOfWork unitOfWork, T entity, string userId, string userIp)
         {
             this.unitOfWork = unitOfWork;
-            this.production = production;
+            this.entity = entity;
             this.userId = userId;
             this.userIp = userIp;
-        }
-
-        public void AddHistory(HistoryEditProperty property, object newValue, HistoryStatus status = HistoryStatus.ToApply)
-        {
-            var applier = HistoryApplierFactory.Get(property);
-
-            var dbhistory = applier.CreateHistory(property, HistoryEntity.Production, production, newValue, status);
-
-            if (dbhistory.NewValue == dbhistory.OldValue)
-                return;
-
-            dbhistory.AffectedProductionId = production.ProductionId;
-            dbhistory.AffectedGroupId = null;
-            dbhistory.UserId = userId;
-            dbhistory.IpAdress = userIp;
-
-            history.Add(dbhistory);
         }
 
         public void Apply()
@@ -56,7 +41,7 @@ namespace C64.Data.History
                 if (hist.Status != HistoryStatus.Applied)
                 {
                     var applier = HistoryApplierFactory.Get(hist.Property);
-                    applier.Apply(production, hist);
+                    applier.Apply(entity, hist);
 
                     hist.Status = HistoryStatus.Applied;
                     hist.Applied = DateTime.Now;
@@ -92,81 +77,51 @@ namespace C64.Data.History
         }
     }
 
-    public class GroupHistoryHandler : IHistoryHandler
+    public class ProductionHistoryHandler : HistoryHandler<Production>
     {
-        private readonly IUnitOfWork unitOfWork;
-        private readonly Group group;
-        private readonly string userId;
-        private readonly string userIp;
-        private readonly List<HistoryRecord> history = new List<HistoryRecord>();
-
-        public GroupHistoryHandler(IUnitOfWork unitOfWork, Group group, string userId, string userIp)
+        public ProductionHistoryHandler(IUnitOfWork unitOfWork, Production production, string userId, string userIp) : base(unitOfWork, production, userId, userIp)
         {
-            this.unitOfWork = unitOfWork;
-            this.group = group;
-            this.userId = userId;
-            this.userIp = userIp;
         }
 
-        public void AddHistory(HistoryEditProperty property, object newValue, HistoryStatus status = HistoryStatus.ToApply)
+        public override void AddHistory(HistoryEditProperty property, object newValue, HistoryStatus status = HistoryStatus.ToApply)
         {
             var applier = HistoryApplierFactory.Get(property);
 
-            var dbhistory = applier.CreateHistory(property, HistoryEntity.Group, group, newValue, status);
+            var dbhistory = applier.CreateHistory(property, HistoryEntity.Production, entity, newValue, status);
 
             if (dbhistory.NewValue == dbhistory.OldValue)
                 return;
 
-            dbhistory.AffectedProductionId = null;
-            dbhistory.AffectedGroupId = group.GroupId;
+            dbhistory.AffectedProductionId = entity.ProductionId;
+            dbhistory.AffectedGroupId = null;
             dbhistory.UserId = userId;
             dbhistory.IpAdress = userIp;
 
             history.Add(dbhistory);
         }
+    }
 
-        public void Apply()
+    public class GroupHistoryHandler : HistoryHandler<Group>
+    {
+        public GroupHistoryHandler(IUnitOfWork unitOfWork, Group group, string userId, string userIp) : base(unitOfWork, group, userId, userIp)
         {
-            var transactionId = Guid.NewGuid().ToString();
-
-            foreach (var hist in history)
-            {
-                if (hist.Status != HistoryStatus.Applied)
-                {
-                    var applier = HistoryApplierFactory.Get(hist.Property);
-                    applier.Apply(group, hist);
-
-                    hist.Status = HistoryStatus.Applied;
-                    hist.Applied = DateTime.Now;
-                    hist.TransactionId = transactionId;
-                    unitOfWork.Productions.AddHistory(hist);
-                }
-            };
         }
 
-        public void Undo(IEnumerable<HistoryRecord> historiesToUndo)
+        public override void AddHistory(HistoryEditProperty property, object newValue, HistoryStatus status = HistoryStatus.ToApply)
         {
-            var transactionId = Guid.NewGuid().ToString();
-            foreach (var toUndo in historiesToUndo.OrderByDescending(p => p.Applied))
-            {
-                history.Add(new HistoryRecord
-                {
-                    AffectedEntity = toUndo.AffectedEntity,
-                    AffectedProductionId = toUndo.AffectedProductionId,
-                    AffectedGroupId = toUndo.AffectedGroupId,
-                    AffectedScenerId = toUndo.AffectedScenerId,
-                    Applied = DateTime.Now,
-                    NewValue = toUndo.OldValue,
-                    OldValue = toUndo.NewValue,
-                    Property = toUndo.Property,
-                    Type = toUndo.Type,
-                    TransactionId = transactionId,
-                    UserId = userId,
-                    IpAdress = userIp,
-                });
-                toUndo.Undid = DateTime.Now;
-                toUndo.Status = HistoryStatus.Undid;
-            }
+            var applier = HistoryApplierFactory.Get(property);
+
+            var dbhistory = applier.CreateHistory(property, HistoryEntity.Group, entity, newValue, status);
+
+            if (dbhistory.NewValue == dbhistory.OldValue)
+                return;
+
+            dbhistory.AffectedProductionId = null;
+            dbhistory.AffectedGroupId = entity.GroupId;
+            dbhistory.UserId = userId;
+            dbhistory.IpAdress = userIp;
+
+            history.Add(dbhistory);
         }
     }
 
