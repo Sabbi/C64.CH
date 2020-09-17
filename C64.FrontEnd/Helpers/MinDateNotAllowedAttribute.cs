c52a2fe6d364579fbc2adbe925549d20840df3b1
Damian <d@c64.ch>
2020-09-17 23:15:04 +0200

    Update MinDateNotAllowed
    still does not behave as it should (only validates when changed)

--- C64.FrontEnd/Helpers/MinDateNotAllowedAttribute.cs
+++ C64.FrontEnd/Helpers/MinDateNotAllowedAttribute.cs
@@ -7,13 +7,18 @@ namespace C64.FrontEnd.Helpers
     {
         protected override ValidationResult IsValid(object value, ValidationContext validationContext)
         {
+            return IsValid(value) ? ValidationResult.Success : new ValidationResult($"The {validationContext.DisplayName} field is not a valid date");
+        }
+
+        public override bool IsValid(object value)
+        {
             if (value != null && value.GetType() == typeof(DateTime))
             {
-                if ((DateTime)value != DateTime.MinValue)
-                    return ValidationResult.Success;
+                if ((DateTime)value != DateTime.MinValue && ((DateTime)value).Year > 1900)
+                    return true;
             }
 
-            return new ValidationResult($"The {validationContext.DisplayName} field is not a valid date");
+            return false;
         }
     }
 }
