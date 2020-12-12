235193e32de5cec50dc2953e619ce08908dc8bdf
Damian <d@c64.ch>
2020-12-12 15:19:36 +0100

    Map all the old URLs
    fixes #141

---
+++ C64.FrontEnd/Controllers/RedirectController.cs
@@ -0,0 +1,171 @@
+﻿using C64.Data;
+using C64.FrontEnd.Extensions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Threading.Tasks;
+
+namespace C64.FrontEnd.Controllers
+{
+    public class RedirectController : Controller
+    {
+        private readonly IUnitOfWork unitOfWork;
+        private readonly IHttpContextAccessor httpContextAccessor;
+
+        private bool permanentRedirect { get; set; }
+
+        public RedirectController(IConfiguration config, IUnitOfWork unitOfWork, IHttpContextAccessor httpContextAccessor)
+        {
+            permanentRedirect = config.GetValue<bool>("DoPermanentRedirect");
+            this.unitOfWork = unitOfWork;
+            this.httpContextAccessor = httpContextAccessor;
+        }
+
+        [Route("/demos")]
+        public IActionResult Demos()
+        {
+            return DoRedirect("/productions/demos");
+        }
+
+        // /about -> /help/about
+        [Route("/about")]
+        public IActionResult About()
+        {
+            return DoRedirect("/help/about");
+        }
+
+        // /programming/index.php	/programming
+        [Route("/programming/index.php")]
+        public IActionResult Programming()
+        {
+            return DoRedirect("/programming");
+        }
+
+        // /contact -> /help/contact
+        [Route("/contact")]
+        public IActionResult Contact()
+        {
+            return DoRedirect("/help/contact");
+        }
+
+        // /demos/parties.php -> /parties
+        [Route("/demos/parties.php")]
+        public IActionResult Parties()
+        {
+            return DoRedirect("/parties");
+        }
+
+        // /demos/realdetail.php?id=x -> /productions/{id}/{name}
+        [Route("/demos/realdetail.php")]
+        public async Task<IActionResult> DemoDetail(int id)
+        {
+            var demo = await unitOfWork.Productions.Get(id);
+            if (demo != null)
+                return DoRedirect($"/productions/{id}/{demo.Name.UrlEncode()}");
+
+            return DoRedirect($"/productions/{id}");
+        }
+
+        // /user/personal.php	/favorites/{userId}
+        [Route("/user/personal.php")]
+        public IActionResult Personal()
+        {
+            var userId = httpContextAccessor.HttpContext.GetUserId();
+            if (!string.IsNullOrEmpty(userId))
+                return DoRedirect($"/favorites/{userId}");
+            else
+                return DoRedirect($"/account");
+        }
+
+        // /demos/list.php?group=x&source=group -> /groups/{id}/{name}
+        // /demos/list.php?source=toprated -> /productions/demos/toprated
+        // /demos/list.php?source=topdownloaded -> /productions/demos/topdownloaded
+        // /demos/list.php?source=latest -> /productions/demos/latestadded
+        // /demos/list.php?year=x&source=year -> /productions/demos/year/{year}
+        // /demos/list.php?groupname=x&source=groupname -> /productions/demos/groupstart/{letter}
+        // /demos/list.php?demoname=x&source=demoname -> /productions/demos/namestart/{letter}
+        // /demos/list.php?search=search&source=search -> /search/?search={search}
+        [Route("/demos/list.php")]
+        public async Task<IActionResult> List(string source, int group = 0, string demoname = null, string groupname = null, int year = 0, string search = null)
+        {
+            source = source?.ToLower();
+
+            switch (source)
+            {
+                case "group":
+                    var groupData = await unitOfWork.Groups.Get(group);
+                    if (groupData != null)
+                        return DoRedirect($"/groups/{group}/{groupData.Name}");
+                    else
+                        return DoRedirect($"/groups/{group}");
+
+                case "toprated":
+                    return DoRedirect("/productions/demos/toprated");
+
+                case "topdownloaded":
+                    return DoRedirect("/productions/demos/topdownloaded");
+
+                case "latest":
+                    return DoRedirect("/productions/demos/latestadded");
+
+                case "year":
+                    return DoRedirect($"/productions/demos/year/{year}");
+
+                case "groupname":
+                    return DoRedirect($"/productions/demos/groupstart/{groupname}");
+
+                case "demoname":
+                    return DoRedirect($"/productions/demos/namestart/{demoname}");
+
+                case "search":
+                    return DoRedirect($"/search/?search={search}");
+
+                default:
+                    throw new ArgumentException($"Source {source} invalid");
+            }
+        }
+
+        // /user/profile.php -> /account/publicprofile
+        [Route("/user/profile.php")]
+        public IActionResult AccountPublicProfile()
+        {
+            return DoRedirect($"/account/publicprofile");
+        }
+
+        // /register/lostpw.php -> /account/forgotpassword
+        [Route("/user/changepw.php")]
+        public IActionResult AccountForgotPassword()
+        {
+            return DoRedirect($"/account/forgotpassword");
+        }
+
+        // /user/changepw.php -> /account/password
+        [Route("/user/changepw.php")]
+        public IActionResult AccountChangePw()
+        {
+            return DoRedirect($"/account/password");
+        }
+
+        // /register -> /account/register
+        [Route("/register")]
+        public IActionResult AccountRegister()
+        {
+            return DoRedirect($"/account/register");
+        }
+
+        // /user/changeemail.php -> /account
+        // /user/default.php -> /account
+        [Route("/user/changeemail.php")]
+        [Route("/user/default.php")]
+        public IActionResult Account()
+        {
+            return DoRedirect($"/account");
+        }
+
+        private IActionResult DoRedirect(string url)
+        {
+            return permanentRedirect ? RedirectPermanent(url) : Redirect(url);
+        }
+    }
+}
---
+++ C64.Tests/RedirectTests.cs
@@ -0,0 +1,260 @@
+﻿using C64.Data;
+using C64.Data.Entities;
+using C64.FrontEnd.Controllers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Moq;
+using System.IO;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace C64.Tests
+{
+    public class RedirectTests
+    {
+        private Mock<IUnitOfWork> unitOfWorkMock;
+
+        public RedirectTests()
+        {
+            unitOfWorkMock = new Mock<IUnitOfWork>();
+            unitOfWorkMock.Setup(p => p.Productions.Get(It.IsAny<int>())).ReturnsAsync(new Production { Id = 1, Name = "TestProduction" });
+            unitOfWorkMock.Setup(p => p.Groups.Get(It.IsAny<int>())).ReturnsAsync(new Group { Id = 1, Name = "TestGroup" });
+        }
+
+        private RedirectController CreateController(bool loggedIn = true)
+        {
+            var appSettings = "{\"DoPermanentRedirect\": false}";
+            var builder = new ConfigurationBuilder();
+            builder.AddJsonStream(new MemoryStream(Encoding.UTF8.GetBytes(appSettings)));
+            var config = builder.Build();
+
+            var mockHttpContextAccessor = new Mock<IHttpContextAccessor>();
+
+            mockHttpContextAccessor.Object.HttpContext = new DefaultHttpContext();
+
+            var claim = new Mock<Claim>(ClaimTypes.NameIdentifier, "11111111-1111-1111-111111111111");
+
+            var claimPrinzipal = new Mock<ClaimsPrincipal>();
+
+            if (loggedIn)
+                claimPrinzipal.Setup(p => p.FindFirst(ClaimTypes.NameIdentifier)).Returns(claim.Object);
+
+            mockHttpContextAccessor.Setup(p => p.HttpContext.User).Returns(claimPrinzipal.Object);
+
+            return new RedirectController(config, unitOfWorkMock.Object, mockHttpContextAccessor.Object);
+        }
+
+        [Fact]
+        // /demos -> /productions/demos
+        public void Demos_main()
+        {
+            var redirectController = CreateController();
+            var result = (RedirectResult)redirectController.Demos();
+
+            Assert.False(result.Permanent);
+            Assert.Equal("/productions/demos", result.Url);
+        }
+
+        [Fact]
+        // /demos/realdetail.php?id=x -> /productions/{id}/{name}
+        public async Task Demo_detail()
+        {
+            var redirectController = CreateController();
+            var result = (RedirectResult)await redirectController.DemoDetail(1);
+
+            Assert.Equal("/productions/1/TestProduction", result.Url);
+        }
+
+        [Fact]
+        // /demos/list.php?group=x&source=group -> /groups/{id}/{name}
+        public async Task List_by_group()
+        {
+            var redirectController = CreateController();
+            var result = (RedirectResult)await redirectController.List("group", group: 1);
+            Assert.Equal("/groups/1/TestGroup", result.Url);
+        }
+
+        [Fact]
+        // /demos/list.php?demoname=x&source=demoname -> /productions/demos/namestart/{letter}
+        public async Task List_by_starting_letter()
+        {
+            var redirectController = CreateController();
+            var result = (RedirectResult)await redirectController.List("demoname", demoname: "a");
+            Assert.Equal("/productions/demos/namestart/a", result.Url);
+        }
+
+        [Fact]
+        // /demos/list.php?groupname=x&source=groupname -> /productions/demos/groupstart/{letter}
+        public async Task List_by_starting_letter_group()
+        {
+            var redirectController = CreateController();
+            var result = (RedirectResult)await redirectController.List("groupname", groupname: "a");
+            Assert.Equal("/productions/demos/groupstart/a", result.Url);
+        }
+
+        [Fact]
+        // /demos/list.php?year=x&source=year -> /productions/demos/year/{year}
+        public async Task List_by_year()
+        {
+            var redirectController = CreateController();
+            var result = (RedirectResult)await redirectController.List("year", year: 1999);
+            Assert.Equal("/productions/demos/year/1999", result.Url);
+        }
+
+        [Fact]
+        // /demos/list.php?search=search&source=search -> /search/?search={search}
+        public async Task List_by_search()
+        {
+            var redirectController = CreateController();
+            var result = (RedirectResult)await redirectController.List("search", search: "leterm");
+            Assert.Equal("/search/?search=leterm", result.Url);
+        }
+
+        [Fact]
+        // /demos/list.php?source=latest -> /productions/demos/latestadded
+        public async Task Latest_Demos()
+        {
+            var redirectController = CreateController();
+            var result = (RedirectResult)await redirectController.List("latest");
+            Assert.Equal("/productions/demos/latestadded", result.Url);
+        }
+
+        [Fact]
+        // /demos/list.php?source=topdownloaded -> /productions/demos/topdownloaded
+        public async Task Top_Downloaded()
+        {
+            var redirectController = CreateController();
+            var result = (RedirectResult)await redirectController.List("topdownloaded");
+            Assert.Equal("/productions/demos/topdownloaded", result.Url);
+        }
+
+        [Fact]
+        // /demos/list.php?source=toprated -> /productions/demos/toprated
+        public async Task Top_Voted()
+        {
+            var redirectController = CreateController();
+            var result = (RedirectResult)await redirectController.List("toprated");
+            Assert.Equal("/productions/demos/toprated", result.Url);
+        }
+
+        [Fact]
+        // /programming/index.php	/programming
+        public void Programming()
+        {
+            var redirectController = CreateController();
+            var result = (RedirectResult)redirectController.Programming();
+
+            Assert.Equal("/programming", result.Url);
+        }
+
+        [Fact]
+        // /demos/parties.php -> /parties
+        public void Parties()
+        {
+            var redirectController = CreateController();
+            var result = (RedirectResult)redirectController.Parties();
+
+            Assert.Equal("/parties", result.Url);
+        }
+
+        [Fact]
+        // /contact -> /help/contact
+        public void Contact()
+        {
+            var redirectController = CreateController();
+            var result = (RedirectResult)redirectController.Contact();
+
+            Assert.Equal("/help/contact", result.Url);
+        }
+
+        [Fact]
+        // /about -> /help/about
+        public void About()
+        {
+            var redirectController = CreateController();
+            var result = (RedirectResult)redirectController.About();
+
+            Assert.Equal("/help/about", result.Url);
+        }
+
+        [Fact]
+        // /register/	/account/register
+        public void Register()
+        {
+            var redirectController = CreateController();
+            var result = (RedirectResult)redirectController.AccountRegister();
+
+            Assert.Equal("/account/register", result.Url);
+        }
+
+        [Fact]
+        // /register/lostpw.php	/account/forgotpassword
+        public void Forgot_Password()
+        {
+            var redirectController = CreateController();
+            var result = (RedirectResult)redirectController.AccountForgotPassword();
+
+            Assert.Equal("/account/forgotpassword", result.Url);
+        }
+
+        [Fact]
+        // /user/default.php	/account
+        public void Control_center()
+        {
+            var redirectController = CreateController();
+            var result = (RedirectResult)redirectController.Account();
+
+            Assert.Equal("/account", result.Url);
+        }
+
+        [Fact]
+        // /user/profile.php	/account/publicprofile
+        public void Edit_profile()
+        {
+            var redirectController = CreateController();
+            var result = (RedirectResult)redirectController.AccountPublicProfile();
+
+            Assert.Equal("/account/publicprofile", result.Url);
+        }
+
+        [Fact]
+        // /user/personal.php	/favorites/{userId}
+        public void Personal()
+        {
+            var controller = CreateController();
+            var result = (RedirectResult)controller.Personal();
+            Assert.True(result.Url == "/favorites/11111111-1111-1111-111111111111");
+        }
+
+        [Fact]
+        // /user/personal.php	/favorites/{userId}
+        public void PersonalNotLoggedIn()
+        {
+            var controller = CreateController(false);
+            var result = (RedirectResult)controller.Personal();
+            Assert.Equal("/account", result.Url);
+        }
+
+        [Fact]
+        // /user/changepw.php	/account/password
+        public void Change_password()
+        {
+            var controller = CreateController(false);
+            var result = (RedirectResult)controller.AccountChangePw();
+            Assert.Equal("/account/password", result.Url);
+        }
+
+        [Fact]
+        // /user/changeemail.php	/account
+        public void Change_email()
+        {
+            var redirectController = CreateController();
+            var result = (RedirectResult)redirectController.Account();
+
+            Assert.Equal("/account", result.Url);
+        }
+    }
+}
