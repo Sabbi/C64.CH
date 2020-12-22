f12e42bdac466135ada5aafc84e8979dad5c6bbc
Damian <d@c64.ch>
2020-12-22 08:52:58 +0100

    Initial

---
+++ Seeder/Pages/Seed_Misc.cs
@@ -0,0 +1,74 @@
+﻿using C64.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Seeder.Pages
+{
+    public partial class Seed
+    {
+        private async Task AddGuestbook()
+        {
+            var rnd = new Random();
+            for (var i = 0; i < 20; i++)
+            {
+                var rs = GetRandomStuff();
+                var entry = new GuestbookEntry
+                {
+                    Comment = rs.Text,
+                    Homepage = rs.Url,
+                    Name = rs.Handle,
+                    WrittenAt = DateTime.Now.AddDays(rnd.Next(-365, 0)),
+                    Ip = RandomIp(),
+                };
+
+                await dbContext.GuestbookEntries.AddAsync(entry);
+            }
+        }
+
+        private async Task AddLinks()
+        {
+            var rnd = new Random();
+            for (var i = 0; i < 15; i++)
+            {
+                var rs = GetRandomStuff();
+                var entry = new Link
+                {
+                    LinkCategoryId = rnd.Next(1, 6),
+                    Url = rs.Url,
+                    Name = rs.Handle,
+                    Added = DateTime.Now.AddDays(rnd.Next(-365, 0)),
+                };
+
+                await dbContext.Links.AddAsync(entry);
+            }
+        }
+
+        private async Task AddSiteNews()
+        {
+            var rnd = new Random();
+            for (var i = 0; i < 10; i++)
+            {
+                var rs = GetRandomStuff();
+                var entry = new SiteInfo
+                {
+                    PublishedAt = DateTime.Now.AddDays(rnd.Next(-365, 0)),
+                    Show = true,
+                    Text = rs.Text,
+                    Title = rs.Text.Substring(0, 10),
+                    Writer = rs.Handle
+                };
+                await dbContext.SiteInfos.AddAsync(entry);
+            }
+        }
+
+        private async Task AddTools()
+        {
+            var json = System.IO.File.ReadAllText("Data/tools.json");
+            var parsed = JsonSerializer.Deserialize<List<Tool>>(json, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+
+            await dbContext.Tools.AddRangeAsync(parsed);
+        }
+    }
+}
---
+++ Seeder/Pages/Seed_Users.cs
@@ -0,0 +1,40 @@
+﻿using C64.Data.Entities;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+
+namespace Seeder.Pages
+{
+    public partial class Seed
+    {
+        private async Task AddRoles()
+        {
+            var adminRole = new IdentityRole("Admin");
+            var moderatorRole = new IdentityRole("Moderator");
+            await roleManager.CreateAsync(adminRole);
+            await roleManager.CreateAsync(moderatorRole);
+        }
+
+        private async Task AddAdminUser()
+        {
+            var user = new User
+            {
+                UserName = model.AdminHandle,
+                Email = model.AdminEmail,
+            };
+
+            var result = await userManager.CreateAsync(user, model.AdminPassword);
+            await userManager.AddToRolesAsync(user, new string[] { "Admin", "Moderator" });
+        }
+
+        private async Task AddUsers()
+        {
+            for (var i = 1; i < 10; i++)
+            {
+                var guid = $"{i}{i}{i}{i}{i}{i}{i}{i}-{i}{i}{i}{i}-{i}{i}{i}{i}-{i}{i}{i}{i}-{i}{i}{i}{i}{i}{i}{i}{i}{i}{i}{i}{i}";
+                userIds.Add(guid);
+                var user = new User { Id = guid, UserName = $"TestUser{i}", Email = $"TestUser{i}@sampledb.com" };
+                var result = await userManager.CreateAsync(user, "123456");
+            }
+        }
+    }
+}
