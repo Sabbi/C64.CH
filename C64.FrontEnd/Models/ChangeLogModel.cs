8d5e9cf5b39dc0f73c6ed6dc49eeb5bb011ab48c
Damian <d@c64.ch>
2020-12-27 16:03:21 +0100

    Simple changelog

---
+++ C64.FrontEnd/Models/ChangeLogModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace C64.FrontEnd.Models
+{
+    public class ChangeLogModel
+    {
+        public DateTime Date { get; set; }
+        public string Version { get; set; }
+
+        public IEnumerable<string> Changes { get; set; }
+
+        public ChangeLogModel()
+        {
+        }
+
+        public ChangeLogModel(DateTime date, string version, params string[] changes)
+        {
+            Date = date;
+            Version = version;
+            Changes = changes;
+        }
+    }
+}
