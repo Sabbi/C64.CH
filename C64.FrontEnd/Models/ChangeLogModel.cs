277ba605ba61dc3dc98c7c027260ed070b190e73
Damian <d@c64.ch>
2021-01-15 21:42:15 +0100

    Get rid of version in changelog

--- C64.FrontEnd/Models/ChangeLogModel.cs
+++ C64.FrontEnd/Models/ChangeLogModel.cs
@@ -6,18 +6,15 @@ namespace C64.FrontEnd.Models
     public class ChangeLogModel
     {
         public DateTime Date { get; set; }
-        public string Version { get; set; }
-
         public IEnumerable<string> Changes { get; set; }
 
         public ChangeLogModel()
         {
         }
 
-        public ChangeLogModel(DateTime date, string version, params string[] changes)
+        public ChangeLogModel(DateTime date, params string[] changes)
         {
             Date = date;
-            Version = version;
             Changes = changes;
         }
     }
