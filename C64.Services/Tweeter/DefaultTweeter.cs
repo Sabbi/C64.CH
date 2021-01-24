e6bb19f26dafe014135402edd9801ebd9b04e865
Damian <d@c64.ch>
2021-01-24 17:11:47 +0100

    Setup Tweetinvi

---
+++ C64.Services/Tweeter/DefaultTweeter.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+using Tweetinvi;
+using Tweetinvi.Models;
+
+namespace C64.Services.Tweeter
+{
+    public class DefaultTweeter : ITweeter
+    {
+        private readonly ILogger<DefaultTweeter> logger;
+        private TwitterClient client;
+
+        public DefaultTweeter(string consumerKey, string consumerSecret, string accessToken, string accessTokenSecret, ILogger<DefaultTweeter> logger)
+        {
+            var credentials = new TwitterCredentials(consumerKey, consumerSecret, accessToken, accessTokenSecret);
+            client = new TwitterClient(credentials);
+            this.logger = logger;
+        }
+
+        public async Task SendTweet(string text)
+        {
+            var result = await client.Tweets.PublishTweetAsync(text);
+            logger.LogInformation("Sent tweet {text}, Result {@result}", text, result);
+        }
+    }
+}
