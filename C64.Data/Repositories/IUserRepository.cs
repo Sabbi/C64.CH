32a364a41ff1327696340937b4f686e76ec3d908
Damian <d@c64.ch>
2021-02-02 20:18:58 +0100

    Add EditHistory-paginator
    (unclean)

--- C64.Data/Repositories/IUserRepository.cs
+++ C64.Data/Repositories/IUserRepository.cs
@@ -1,10 +1,14 @@
 ﻿using C64.Data.Entities;
+using C64.Data.Models;
+using System;
+using System.Linq.Expressions;
 using System.Threading.Tasks;
 
 namespace C64.Data.Repositories
 {
     public interface IUserRepository : IRepository<User>
     {
+        Task<PaginatedResult<HistoryRecord>> GetPaginatedHistory(Expression<Func<HistoryRecord, bool>> predicate, string orderBy, bool isSortedAscending, int page, int pageSize);
         Task<User> GetWithFavorites(string userId);
         Task<bool> IsFavorite(string userId, int productionId);
         void SetFavorite(string userId, int productionId, bool set);
