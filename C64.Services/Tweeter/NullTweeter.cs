6deb293451c8decbe66f797ac1fb3db3840e52e5
Damian <d@c64.ch>
2021-02-12 20:31:26 +0100

    Pimp up Tweeter service

--- C64.Services/Tweeter/NullTweeter.cs
+++ C64.Services/Tweeter/NullTweeter.cs
@@ -2,8 +2,13 @@
 
 namespace C64.Services.Tweeter
 {
-    public class NullTweeter : ITweeter
+    public class NullTweeter : ITweeter, IPictureTweeter
     {
+        public Task SendPictureTweet(string text, byte[] pictureData)
+        {
+            return Task.FromResult(0);
+        }
+
         public Task SendTweet(string text)
         {
             return Task.FromResult(0);
