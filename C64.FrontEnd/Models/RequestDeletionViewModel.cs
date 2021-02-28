0e430b8062ee70f63f989d75792477f54b777a7e
Damian <d@c64.ch>
2021-02-28 13:02:59 +0100

    Add request deletion to production and scener

---
+++ C64.FrontEnd/Models/RequestDeletionViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace C64.FrontEnd.Models
+{
+    public class RequestDeletionViewModel
+    {
+        [Required]
+        [MaxLength(255)]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [MaxLength(255)]
+        public string Email { get; set; }
+
+        [Required]
+        [MaxLength(4095)]
+        public string Comment { get; set; }
+    }
+}
