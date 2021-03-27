4131778268e3915e8d4eac73afe0eedbf30bbe76
Damian <d@c64.ch>
2021-03-27 10:25:00 +0100

    Move unspecified credit to the end

---
+++ C64.FrontEnd/Extensions/ProductionCreditExtensions.cs
@@ -0,0 +1,17 @@
+﻿using C64.Data;
+using C64.Data.Entities;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace C64.FrontEnd.Extensions
+{
+    public static class ProductionCreditExtensions
+    {
+        public static ICollection<ProductionCredit> GetSorted(this ICollection<ProductionCredit> credits)
+        {
+            var retVal = credits.Where(p => p.Credit != Credit.Unspecified).OrderBy(p => p.Credit).ToList();
+            retVal.AddRange(credits.Where(p => p.Credit == Credit.Unspecified));
+            return retVal;
+        }
+    }
+}
