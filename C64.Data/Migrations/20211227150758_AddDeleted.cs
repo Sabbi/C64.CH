5fb8428d4d06db4cadd75cf790c8efac3f2b4586
Damian <d@c64.ch>
2021-12-28 10:01:16 +0100

    Add deleted-flag to relevant models

---
+++ C64.Data/Migrations/20211227150758_AddDeleted.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace C64.Data.Migrations
+{
+    public partial class AddDeleted : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Deleted",
+                table: "sceners",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Deleted",
+                table: "productions",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Deleted",
+                table: "parties",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Deleted",
+                table: "groups",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Deleted",
+                table: "sceners");
+
+            migrationBuilder.DropColumn(
+                name: "Deleted",
+                table: "productions");
+
+            migrationBuilder.DropColumn(
+                name: "Deleted",
+                table: "parties");
+
+            migrationBuilder.DropColumn(
+                name: "Deleted",
+                table: "groups");
+        }
+    }
+}
---
+++ C64.Data/Migrations/20211227153859_DeleteIndices.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace C64.Data.Migrations
+{
+    public partial class DeleteIndices : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_sceners_Deleted",
+                table: "sceners",
+                column: "Deleted");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_productions_Deleted",
+                table: "productions",
+                column: "Deleted");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_parties_Deleted",
+                table: "parties",
+                column: "Deleted");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_groups_Deleted",
+                table: "groups",
+                column: "Deleted");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_sceners_Deleted",
+                table: "sceners");
+
+            migrationBuilder.DropIndex(
+                name: "IX_productions_Deleted",
+                table: "productions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_parties_Deleted",
+                table: "parties");
+
+            migrationBuilder.DropIndex(
+                name: "IX_groups_Deleted",
+                table: "groups");
+        }
+    }
+}
